a6b1ceb536a389ba2d43a03e920256afe72b4fa2
zdy <3460836754@qq.com>
2023-12-18 17:28:36 +0000

    Zdy finished 3 version Gemm. ld.shared.v4.f32 has bug.

--- gemm1.cu
+++ gemm1.cu
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <cuda_runtime.h>
 
-const int M = 20480;
-const int N = 4096;
-const int K = 4096;
+constexpr int M = 20480;
+constexpr int N = 4096;
+constexpr int K = 4096;
+
+#define DEBUG_M_TILEx
+#define BUG
+#define TO_PTXx
+
+// #define CUDA_CALL(k) { \
+//   cudaError_t e = (k); \
+//   if (e != cudaSuccess) { \
+//     std::cerr << "CUDA error in " << __FILE__ << " at line " << __LINE__ \
+//               << ": " << cudaGetErrorString(err) << std::endl; \
+//   } \
+// }
 
 void fill_mat(float* mat, const int m, const int n) {
+  // size_t tmp = 0;
   for (int i = 0; i < m; ++i) {
     for (int j = 0; j < n; ++j) {
       mat[i * n + j] = 1;
@@ -17,7 +30,7 @@ void check_mat(const float* mat, const int m, const int n, const int k) {
   size_t sum = 0;
   for (int i = 0; i < m; ++i) {
     for (int j = 0; j < n; ++j) {
-      if (mat[i * n + j] != k) {
+      if ((int)mat[i * n + j] != k) {
         std::cout << "Error! index (" << i << ", " << j << ") is " << mat[i * n + j] << std::endl;
         sum++;
       }
@@ -27,6 +40,7 @@ void check_mat(const float* mat, const int m, const int n, const int k) {
     std::cout << "OK." << std::endl;
 }
 
+#ifndef TO_PTX
 /*!
  *  \brief gemm version 0. 1 thread for k MAC
  *  \note gridDim control Matrix size; blockDim Matters performance
@@ -78,11 +92,447 @@ __global__ void gemm1_kernel(const float* A, const float* B, float* C, const int
   /*! \note write partial sum back to global memory */
   C[row * N + col] = pSum;
 }
+#endif // TO_PTX
 
-__global__ void gemm1_kernel(const float* A, const float* B, float* C, const int M, const int N, const int K) {
-  
+/*!  \note 99KB per CTA, 100KB per SM */
+constexpr int M_TILE_SIZE = 128;
+constexpr int N_TILE_SIZE = 128;
+constexpr int K_TILE_SIZE = 8;
+
+constexpr int M_WARP_SIZE = 4;
+constexpr int N_WARP_SIZE = 2;
+
+constexpr int M_THREAD = 4;
+constexpr int N_THREAD = 8;
+
+__device__ __forceinline__ void lds128(
+  float& reg0, float& reg1, float& reg2, float& reg3,
+  const float* addr
+) {
+  asm volatile (
+    "ld.shared.v4.f32 {%0, %1, %2, %3}, [%4];\n"
+    : "=f"(reg0), "=f"(reg1), "=f"(reg2), "=f"(reg3)
+    : "l"(addr)
+  );
 }
 
+__device__ __forceinline__ void stg128(
+  float& reg0, float& reg1, float& reg2, float& reg3,
+  float* addr
+) {
+  asm volatile (
+    "st.global.v4.f32 [%4], {%0, %1, %2, %3};\n"
+    :
+    : "f"(reg0), "f"(reg1), "f"(reg2), "f"(reg3), "l"(addr)
+  );
+}
+__device__ __forceinline__ void null_asm() {
+  asm volatile(";");
+}
+
+
+//TODO bug
+#ifndef BUG
+/*! \brief a block compute 128 * 128 tile */
+__global__ void gemm2_kernel(const float* A, const float* B, float* C, const int M, const int N, const int K) {
+  //TODO 每个CTA应该处理多个block -> 需要两个循环
+  //TODO Mtile & Ntile conflict
+  __shared__ __align__(M_TILE_SIZE * 4) float Mtile[M_TILE_SIZE * K_TILE_SIZE];
+  __shared__ __align__(N_TILE_SIZE * 4) float Ntile[K_TILE_SIZE * N_TILE_SIZE];
+
+  const uint32_t lane_id = threadIdx.x % 32;
+  const uint32_t warp_id = threadIdx.x / 32;
+
+  float A_frag[8];
+  float B_frag[8];
+  float C_frag[8][8];
+  #pragma unroll
+  for (int i = 0; i < 8; ++i) {
+      #pragma unroll
+      for (int j = 0; j < 8; ++j) {
+          C_frag[i][j] = 0.;
+      }
+  }
+
+  /*! \note A_ldg_ptr: start of M tile */
+  const float* A_ldg_ptr = (const float*)(
+    A + (blockIdx.y * M_TILE_SIZE + threadIdx.x / K_TILE_SIZE * 4) * K + threadIdx.x % K_TILE_SIZE
+  );
+  /*! \note B_ldg_ptr: start of N tile */
+  const float* B_ldg_ptr = (const float*)(
+    B + (threadIdx.x / 32) * N + blockIdx.x * N_TILE_SIZE + threadIdx.x % 32
+  );
+
+  float* A_sts_ptr = Mtile + (threadIdx.x % 8) * M_TILE_SIZE + (threadIdx.x / 8) * 4;
+  float* B_sts_ptr = Ntile + (threadIdx.x / 32) * N_TILE_SIZE + (threadIdx.x % 32);
+
+  /*! \todo Zig-zag to avoid bank conflict */
+  const float* A_lds_ptr = (const float*)(
+    Mtile + (K_TILE_SIZE - 1) * M_TILE_SIZE
+    + (warp_id / N_WARP_SIZE) * M_THREAD * 8
+    + (lane_id / N_THREAD) * 4
+  );
+  /*! \todo Zig-zag to avoid bank conflict */
+  const float* B_lds_ptr = (const float*)(
+    Ntile + (warp_id % N_WARP_SIZE) * N_THREAD * 8 + (lane_id % N_THREAD) * 4
+  );
+
+  float* C_stg_ptr = (C 
+    + (blockIdx.y * M_TILE_SIZE // y-dim start of block
+      + warp_id / N_WARP_SIZE * M_THREAD * 8 // y-dim start of warp
+      + lane_id / N_THREAD * 4) * N // y-dim start of thread
+    + (blockIdx.x * N_TILE_SIZE // x-dim start of block 
+      + warp_id % N_WARP_SIZE * N_THREAD * 8 // x-dim start of warp
+      + lane_id % N_THREAD * 4) // x-dim start of thread
+  );
+
+  /*! \note compute Mtile by Ntile by Ktile GEMM */
+  for (int p = 0; p < K / K_TILE_SIZE; ++p) {
+
+    {
+      /*! \note load data into shared memory */
+
+      // load A to Mtile
+      /*! \todo does float4 matters*/
+      float4 a_tmp4;
+      float4* A_sts_tmp4_ptr = (float4*)A_sts_ptr;
+      a_tmp4.x = A_ldg_ptr[0];
+      a_tmp4.y = A_ldg_ptr[K_TILE_SIZE];
+      a_tmp4.z = A_ldg_ptr[K_TILE_SIZE * 2];
+      a_tmp4.w = A_ldg_ptr[K_TILE_SIZE * 3];
+      A_sts_tmp4_ptr[0] = a_tmp4;
+      // load B to Mtile
+      /*! \todo does new asm matters? lgd2s */
+      B_sts_ptr[0]  = B_ldg_ptr[0];
+      B_sts_ptr[32] = B_ldg_ptr[32];
+      B_sts_ptr[64] = B_ldg_ptr[64];
+      B_sts_ptr[96] = B_ldg_ptr[96];
+
+      __syncthreads();
+    }
+
+    /*! \note Warp tile */
+    for (int k = 0; k < TILE_WIDTH; ++k) {
+      // load shmem to reg
+      lds128(
+        A_frag[0], A_frag[1], A_frag[2], A_frag[3],
+        A_lds_ptr
+      );
+      lds128(
+        A_frag[4], A_frag[5], A_frag[6], A_frag[7],
+        A_lds_ptr + M_THREAD * 4
+      );      
+      lds128(
+        B_frag[0], B_frag[1], B_frag[2], B_frag[3],
+        B_lds_ptr
+      );
+      lds128(
+        B_frag[4], B_frag[5], B_frag[6], B_frag[7],
+        B_lds_ptr + N_THREAD * 4
+      );
+
+      A_lds_ptr -= M_TILE_SIZE;
+      B_lds_ptr += N_TILE_SIZE;
+
+      // compute
+      #pragma unroll
+      for (int m = 0; m < 8; ++m) {
+        #pragma unroll
+        for (int n = 0; n < 8; ++n) {
+          C_frag[m][n] += A_frag[m] * B_frag[n];
+        }
+      }
+    }
+    __syncthreads();
+  }
+  
+  /*! \note Write Back */
+  /*! \todo Naive implementation: reg to global memory*/
+  /*! \todo Write to shared memory first, write to global memory second */
+  //TODO 写回
+  #pragma unroll
+  for (int m = 0; m < 4; ++m) {
+    stg128(
+      C_frag[m][0],
+      C_frag[m][1],
+      C_frag[m][2],
+      C_frag[m][3],
+      C_stg_ptr
+    );
+    stg128(
+      C_frag[m][4],
+      C_frag[m][5],
+      C_frag[m][6],
+      C_frag[m][7],
+      C_stg_ptr + 32
+    );
+    C_stg_ptr += N;
+  }
+
+  C_stg_ptr += (M_THREAD - 1) * 4;
+  #pragma unroll
+  for (int m = 5; m < 8; ++m) {
+    stg128(
+      C_frag[m][0],
+      C_frag[m][1],
+      C_frag[m][2],
+      C_frag[m][3],
+      C_stg_ptr
+    );
+    stg128(
+      C_frag[m][4],
+      C_frag[m][5],
+      C_frag[m][6],
+      C_frag[m][7],
+      C_stg_ptr + 32
+    );
+    C_stg_ptr += N;
+  }
+}
+#endif
+
+/*! \brief a block compute 128 * 128 tile */
+__global__ void gemm2_debug_kernel(const float* A, const float* B, float* C, const int M, const int N, const int K, float* D = nullptr) {
+  //* BUG FREE
+  __shared__ __align__(M_TILE_SIZE * 4) float Mtile[M_TILE_SIZE * K_TILE_SIZE];
+  __shared__ __align__(N_TILE_SIZE * 4) float Ntile[K_TILE_SIZE * N_TILE_SIZE];
+
+  const uint32_t lane_id = threadIdx.x % 32;
+  const uint32_t warp_id = threadIdx.x / 32;
+
+  float A_frag[8];
+  float B_frag[8];
+  float C_frag[8][8];
+  #pragma unroll
+  for (int i = 0; i < 8; ++i) {
+      #pragma unroll
+      for (int j = 0; j < 8; ++j) {
+          C_frag[i][j] = 0.;
+      }
+  }
+
+  /*! \note A_ldg_ptr: start of M tile */
+  const float* A_ldg_ptr = (const float*)(
+    A + (blockIdx.y * M_TILE_SIZE + threadIdx.x / K_TILE_SIZE * 4) * K + threadIdx.x % K_TILE_SIZE
+  );
+  /*! \note B_ldg_ptr: start of N tile */
+  const float* B_ldg_ptr = (const float*)(
+    B + (threadIdx.x / 32) * N + blockIdx.x * N_TILE_SIZE + threadIdx.x % 32
+  );  
+
+  //* End BUG FREE
+
+  float* C_stg_ptr = (C 
+    + (blockIdx.y * M_TILE_SIZE // y-dim start of block
+      + warp_id / N_WARP_SIZE * M_THREAD * 8 // y-dim start of warp
+      + lane_id / N_THREAD * 4) * N // y-dim start of thread
+    + (blockIdx.x * N_TILE_SIZE // x-dim start of block 
+      + warp_id % N_WARP_SIZE * N_THREAD * 8 // x-dim start of warp
+      + lane_id % N_THREAD * 4) // x-dim start of thread
+  );
+
+  /*! \note compute Mtile by Ntile by Ktile GEMM */
+  for (int p = 0; p < K / K_TILE_SIZE; ++p) {
+
+    float* A_sts_ptr = Mtile + (threadIdx.x % 8) * M_TILE_SIZE + (threadIdx.x / 8) * 4;
+    float* B_sts_ptr = Ntile + (threadIdx.x / 32) * N_TILE_SIZE + (threadIdx.x % 32);
+    
+    /*! \todo Zig-zag to avoid bank conflict */
+    const float* A_lds_ptr = (const float*)(Mtile
+      + (warp_id / N_WARP_SIZE) * M_THREAD * 8 // start of m_frag
+      + (lane_id / N_THREAD) * 4
+    );
+    /*! \todo Zig-zag to avoid bank conflict */
+    const float* B_lds_ptr = (const float*)(Ntile 
+      + (warp_id % N_WARP_SIZE) * N_THREAD * 8 
+      + (lane_id % N_THREAD) * 4
+    );    
+
+    {
+      //* BUG FREE
+      /*! \note load data into shared memory */
+
+      // load A to Mtile
+      /*! \todo does float4 matters*/
+      float4 a_tmp4;
+      float4* A_sts_tmp4_ptr = (float4*)A_sts_ptr;
+      a_tmp4.x = A_ldg_ptr[0];
+      a_tmp4.y = A_ldg_ptr[K];
+      a_tmp4.z = A_ldg_ptr[K * 2];
+      a_tmp4.w = A_ldg_ptr[K * 3];
+      A_sts_tmp4_ptr[0] = a_tmp4;
+      // load B to Mtile
+      /*! \todo does new asm matters? lgd2s */
+      B_sts_ptr[0]  = B_ldg_ptr[0];
+      B_sts_ptr[32] = B_ldg_ptr[32];
+      B_sts_ptr[64] = B_ldg_ptr[64];
+      B_sts_ptr[96] = B_ldg_ptr[96];
+
+      __syncthreads();
+      //* end BUG FREE
+    }
+
+    #ifdef DEBUG_M_TILE
+    {
+      if (D != nullptr) {
+        for (int i = 0; i < 4; ++i)
+          D[blockDim.x * i + threadIdx.x] = Mtile[blockDim.x * i + threadIdx.x];
+          __syncthreads();
+      }
+    }
+    #endif
+
+    /*! \note Warp tile */
+    for (int k = 0; k < K_TILE_SIZE; ++k) {
+    #ifdef DEBUG_SHARED_BOUND
+      __syncthreads();
+      printf("%llu:%d\n", threadIdx.x, (A_lds_ptr - Mtile));
+      printf("%llu:%d\n", threadIdx.x, (B_lds_ptr - Ntile));
+      if (threadIdx.x == 0) printf("--------------------------------------\n");
+      __syncthreads();
+    #endif
+      // load shmem to reg
+      //TODO bug
+      #ifndef BUG
+      {
+        lds128(
+          A_frag[0], A_frag[1], A_frag[2], A_frag[3],
+          A_lds_ptr
+        );
+        // lds128(
+        //   A_frag[4], A_frag[5], A_frag[6], A_frag[7],
+        //   A_lds_ptr + M_THREAD * 4
+        // );      
+        lds128(
+          B_frag[0], B_frag[1], B_frag[2], B_frag[3],
+          B_lds_ptr
+        );
+        // lds128(
+        //   B_frag[4], B_frag[5], B_frag[6], B_frag[7],
+        //   B_lds_ptr + N_THREAD * 4
+        // );
+      }
+      #else
+      {
+        float4 tmp4;
+        tmp4 = ((float4*)(A_lds_ptr))[0];
+        A_frag[0] = tmp4.x;
+        A_frag[1] = tmp4.y;
+        A_frag[2] = tmp4.z;
+        A_frag[3] = tmp4.w;
+        tmp4 = ((float4*)(B_lds_ptr))[0];
+        B_frag[0] = tmp4.x;
+        B_frag[1] = tmp4.y;
+        B_frag[2] = tmp4.z;
+        B_frag[3] = tmp4.w;
+        tmp4 = ((float4*)(A_lds_ptr + M_THREAD * 4))[0];
+        A_frag[4] = tmp4.x;
+        A_frag[5] = tmp4.y;
+        A_frag[6] = tmp4.z;
+        A_frag[7] = tmp4.w;
+        tmp4 = ((float4*)(B_lds_ptr + N_THREAD * 4))[0];
+        B_frag[4] = tmp4.x;
+        B_frag[5] = tmp4.y;
+        B_frag[6] = tmp4.z;
+        B_frag[7] = tmp4.w;
+      }
+      #endif
+
+      A_lds_ptr += M_TILE_SIZE;
+      B_lds_ptr += N_TILE_SIZE;
+
+      // compute
+      //* BUG FREE
+      #pragma unroll
+      for (int m = 0; m < 8; ++m) {
+        #pragma unroll
+        for (int n = 0; n < 8; ++n) {
+          C_frag[m][n] += A_frag[m] * B_frag[n];
+        }
+      }
+      //* end BUG FREE
+    }
+
+    A_ldg_ptr += K_TILE_SIZE;
+    B_ldg_ptr += K_TILE_SIZE * N;
+    __syncthreads();
+  }
+
+  /*! \note Write Back */
+  /*! \todo Naive implementation: reg to global memory*/
+  /*! \todo Write to shared memory first, write to global memory second */
+  #pragma unroll
+  for (int m = 0; m < 4; ++m) {
+    #ifndef BUG_FREE
+    {
+      stg128(
+        C_frag[m][0],
+        C_frag[m][1],
+        C_frag[m][2],
+        C_frag[m][3],
+        C_stg_ptr
+      );
+      stg128(
+        C_frag[m][4],
+        C_frag[m][5],
+        C_frag[m][6],
+        C_frag[m][7],
+        C_stg_ptr + 32
+      );    
+    }
+    #else
+    {
+      #pragma unroll
+      for (int n = 0; n < 4; ++n) {
+        C_stg_ptr[n] = C_frag[m][n];
+      }
+      #pragma unroll
+      for (int n = 4; n < 8; ++n) {
+        C_stg_ptr[n + 32 - 4] = C_frag[m][n];
+      }
+    }
+    #endif
+    /*! \note next line of output matrix */
+    C_stg_ptr += N;
+  }
+
+  C_stg_ptr += (M_THREAD - 1) * 4 * N;
+  #pragma unroll
+  for (int m = 4; m < 8; ++m) {
+    #ifndef BUG_FREE
+    {
+      stg128(
+        C_frag[m][0],
+        C_frag[m][1],
+        C_frag[m][2],
+        C_frag[m][3],
+        C_stg_ptr
+      );  
+      stg128(
+        C_frag[m][4],
+        C_frag[m][5],
+        C_frag[m][6],
+        C_frag[m][7],
+        C_stg_ptr + 32
+      );        
+    }
+    #else
+    {
+      #pragma unroll
+      for (int n = 0; n < 4; ++n) {
+        C_stg_ptr[n] = C_frag[m][n];
+      }
+      #pragma unroll
+      for (int n = 4; n < 8; ++n) {
+        C_stg_ptr[n + 32 - 4] = C_frag[m][n];
+      }
+    }
+    #endif
+    C_stg_ptr += N;
+  }
+}
+
+#ifndef TO_PTX
 float* gemm0() {
   float* A = (float*)malloc(M * K * sizeof(float));
   float* B = (float*)malloc(K * N * sizeof(float));
@@ -145,14 +595,54 @@ float* gemm1() {
   free(B);
   return C;
 }
+#endif // TO_PTX
+
+//TODO 分段测试
+float* gemm2() {
+  float* A = (float*)malloc(M * K * sizeof(float));
+  float* B = (float*)malloc(K * N * sizeof(float));
+  float* C = (float*)malloc(M * N * sizeof(float));
+
+  fill_mat(A, M, K);
+  fill_mat(B, K, N);
+
+  float *cuA, *cuB, *cuC;
+  cudaMalloc((void **)&cuA, M * K * sizeof(float));
+  cudaMalloc((void **)&cuB, K * N * sizeof(float));
+  cudaMalloc((void **)&cuC, M * N * sizeof(float));
+
+  cudaMemcpy(cuA, A, M * K * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(cuB, B, K * N * sizeof(float), cudaMemcpyHostToDevice);
+
+  dim3 gridDim(N / N_TILE_SIZE, M / M_TILE_SIZE);
+  gemm2_debug_kernel<<<gridDim, 256>>>(cuA, cuB, cuC, M, N, K);
+  cudaError_t e = cudaDeviceSynchronize();
+  if (e != cudaSuccess) {
+    std::cerr << "CUDA error in " << __FILE__ << " at line " << __LINE__
+              << ": " << cudaGetErrorString(e) << std::endl;
+  }
+
+  cudaMemcpy(C, cuC, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaFree(cuA);
+  cudaFree(cuB);
+  cudaFree(cuC);
+  free(A);
+  free(B);
+  return C;
+}
 
 int main()
 {
+  std::cout << "start..." << std::endl;
   float *mat0 = gemm0();
   check_mat(mat0, M, N, K);
   free(mat0);
   float *mat1 = gemm1();
   check_mat(mat1, M, N, K);
   free(mat1);
+  float* mat2 = gemm2();
+  check_mat(mat2, M, N, K);
+  free(mat2);
   return 0;
 }
